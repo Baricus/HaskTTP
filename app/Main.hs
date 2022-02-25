a14a45cb77faebc8a95c807eacdb1865cd9c4d21
Baricus <31926270+Baricus@users.noreply.github.com>
2022-02-25 11:49:07 -0600

    added port selection and display

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
 
 module Main where
 
@@ -9,6 +9,17 @@ import HTTP
 import Utils
 
 import Data.ByteString (ByteString)
+import System.Environment (getArgs, getProgName)
+import Data.Word (Word16)
+import Text.Read (readMaybe)
+
+-- either returns the default port for no arguments,
+-- the requested port, or Nothing if it could not be parsed
+getPort :: Word16 -> IO (Maybe Word16)
+getPort def = getArgs >>= (\l -> case l of
+                      []       -> pure . pure $ def
+                      (pStr:_) -> pure $ readMaybe pStr
+                    )
 
 -- handle 1 request (a Message and the corresponding ByteString)
 -- subfunctions are implemented in Utils.hs
@@ -20,8 +31,12 @@ processRequest pair = do
     handleHead pair
     handleUnimplemented pair
 
--- launches the server on a port and runs until interrupted
+-- launches the server on a port and runs until interrupted (default port 8000)
 main :: IO ()
-main = runMultiServer 8000 
-            (logAddr *> recvHttp >>= filterFailed processRequest)
-            (toStdOut [" -- ", " : "] <> toFileP "log.txt") 
+main = getPort 8000 >>= (\case 
+            Nothing -> putStr "Usage: " >> (getProgName >>= putStr) >> putStrLn " [port]"
+            Just p  -> putStr "Listening on port " >> print p >>
+                runMultiServer p
+                    (logAddr *> recvHttp >>= filterFailed processRequest)
+                    (toStdOut [" -- ", " : "] <> toFileP "log.txt") 
+        )
